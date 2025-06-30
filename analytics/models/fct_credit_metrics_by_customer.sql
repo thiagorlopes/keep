f7447e7ef23c4b999f04265dc78405f4262b0b61
Thiago Lopes <thiago.lopes@nubank.com.br>
2025-06-30 10:33:51 -0300

    Include new metrics

--- analytics/models/fct_credit_metrics_by_customer.sql
+++ analytics/models/fct_credit_metrics_by_customer.sql
@@ -12,8 +12,26 @@ WITH daily_aggregates AS (
         most_recent_statement_date_minus_180_days,
         SUM(IF(is_revenue, deposits, 0)) as daily_revenue,
         SUM(IF(is_debit, withdrawals, 0)) as daily_debits
-    FROM {{ ref('all_transactions_by_customer') }}
+    FROM main.all_transactions_by_customer
     GROUP BY ALL
+)
+
+,daily_balances AS (
+    SELECT
+        request_id,
+        email,
+        AVG(revised_average_balance) AS revised_average_balance
+    FROM main.fct_daily_transactions_by_customer
+    GROUP BY ALL
+)
+
+,most_recent_balances AS (
+    SELECT
+        request_id,
+        email,
+        balance AS most_recent_balance
+    FROM main. all_transactions_by_customer
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY request_id, email ORDER BY date DESC) = 1
 )
 
 SELECT
@@ -21,6 +39,7 @@ SELECT
     email,
 
     -- Revenue Metrics
+    SUM(daily_revenue) AS revenue_total_credit,
     SUM(daily_revenue) AS revenue_total,
     SUM(IF(date >= most_recent_statement_date_minus_90_days, daily_revenue, 0)) AS revenue_recent_90_days,
     SUM(IF(date >= most_recent_statement_date_minus_180_days AND date < most_recent_statement_date_minus_90_days, daily_revenue, 0)) AS revenue_91_to_180_days,
@@ -28,9 +47,17 @@ SELECT
     -- Debit Metrics
     SUM(daily_debits) AS debits_total,
     SUM(IF(date >= most_recent_statement_date_minus_90_days, daily_debits, 0)) AS debits_recent_90_days,
-    SUM(IF(date >= most_recent_statement_date_minus_180_days AND date < most_recent_statement_date_minus_90_days, daily_debits, 0)) AS debits_91_to_180_days
+    SUM(IF(date >= most_recent_statement_date_minus_180_days AND date < most_recent_statement_date_minus_90_days, daily_debits, 0)) AS debits_91_to_180_days,
 
+    -- Placeholder Credit Metrics
+    0 AS credit_card_payments,
+    0 AS credit_card_recent_90_days,
+    0 AS credit_card_91_to_180_days,
+
+    -- Averages and Balances
+    revised_average_balance AS average_daily_balance_across_bank_accounts,
+    most_recent_balance AS most_recent_balance_across_bank_accounts
 FROM daily_aggregates
-GROUP BY
-    request_id,
-    email
+LEFT JOIN daily_balances USING(request_id, email)
+LEFT JOIN most_recent_balances USING(request_id, email)
+GROUP BY ALL
