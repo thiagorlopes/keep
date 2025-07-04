94b5f67e17f0045406d7e09a632d92bbdca08b96
Thiago Lopes <thiago.lopes@nubank.com.br>
2025-07-04 12:42:49 -0300

    Lines

--- analytics/models/fct_daily_transactions_by_customer.sql
+++ analytics/models/fct_daily_transactions_by_customer.sql
@@ -16,11 +16,11 @@ WITH int_transactions_enriched AS (
         is_revenue,
         is_debit,
         most_recent_statement_date,
-        most_recent_statement_date_minus_30_days,
-        most_recent_statement_date_minus_60_days,
-        most_recent_statement_date_minus_90_days,
-        most_recent_statement_date_minus_180_days,
-        most_recent_statement_date_minus_365_days
+        most_recent_statement_date_minus_30d,
+        most_recent_statement_date_minus_60d,
+        most_recent_statement_date_minus_90d,
+        most_recent_statement_date_minus_180d,
+        most_recent_statement_date_minus_365d
     FROM {{ ref('int_transactions_enriched') }}
 )
 
@@ -97,16 +97,16 @@ WITH int_transactions_enriched AS (
         request_id,
         date,
         -- Use 180 days as default for scaffolding, but this can be easily changed
-        most_recent_statement_date_minus_365_days AS start_date,
+        most_recent_statement_date_minus_365d AS start_date,
         most_recent_statement_date AS end_date,
         
         -- Include all auxiliary date columns for reference
         most_recent_statement_date,
-        most_recent_statement_date_minus_30_days,
-        most_recent_statement_date_minus_60_days,
-        most_recent_statement_date_minus_90_days,
-        most_recent_statement_date_minus_180_days,
-        most_recent_statement_date_minus_365_days
+        most_recent_statement_date_minus_30d,
+        most_recent_statement_date_minus_60d,
+        most_recent_statement_date_minus_90d,
+        most_recent_statement_date_minus_180d,
+        most_recent_statement_date_minus_365d
     FROM int_transactions_enriched
     GROUP BY ALL
 )
@@ -142,6 +142,7 @@ WITH int_transactions_enriched AS (
         email,
         request_id,
         date,
+
         -- Fill forward the last known balance for days without transactions
         LAST_VALUE(average_balance IGNORE NULLS) OVER(
             PARTITION BY email, request_id
@@ -156,7 +157,6 @@ WITH int_transactions_enriched AS (
         email,
         request_id,
         date,
-
         -- Sum of all deposits for the day. This is the "Day Rev" from the sheet.
         SUM(deposits) OVER(PARTITION BY email, request_id, DAYOFYEAR(date)) AS daily_revenue,
     FROM int_transactions_enriched AS trn
@@ -169,7 +169,7 @@ WITH int_transactions_enriched AS (
         email,
         request_id,
         date,
-
+        
         -- Sum of all deposits for the day. This is the "Weekly revenue" from the sheet.
         SUM(deposits) OVER(PARTITION BY email, request_id, WEEKOFYEAR(date)) AS weekly_revenue,
     FROM int_transactions_enriched
@@ -190,11 +190,11 @@ WITH int_transactions_enriched AS (
         
         -- Include auxiliary date columns for reference
         ANY_VALUE(cdr.most_recent_statement_date) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date,
-        ANY_VALUE(cdr.most_recent_statement_date_minus_30_days) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_30_days,
-        ANY_VALUE(cdr.most_recent_statement_date_minus_60_days) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_60_days,
-        ANY_VALUE(cdr.most_recent_statement_date_minus_90_days) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_90_days,
-        ANY_VALUE(cdr.most_recent_statement_date_minus_180_days) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_180_days,
-        ANY_VALUE(cdr.most_recent_statement_date_minus_365_days) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_365_days
+        ANY_VALUE(cdr.most_recent_statement_date_minus_30d) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_30d,
+        ANY_VALUE(cdr.most_recent_statement_date_minus_60d) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_60d,
+        ANY_VALUE(cdr.most_recent_statement_date_minus_90d) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_90d,
+        ANY_VALUE(cdr.most_recent_statement_date_minus_180d) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_180d,
+        ANY_VALUE(cdr.most_recent_statement_date_minus_365d) OVER (PARTITION BY db.email, db.request_id) AS most_recent_statement_date_minus_365d
 
     FROM daily_balances AS db
     LEFT JOIN daily_revenue AS drv USING(email, request_id, date)
